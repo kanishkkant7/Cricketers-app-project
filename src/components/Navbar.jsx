23456e91024c1a2c28e0ca9303e5f03a10212344
kanishkkant7 <kanishk.kant.7@gmail.com>
2025-02-08 23:06:45 +0530

    Added link to App title in navbar with hover animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,17 @@ const Navbar = forwardRef((props, ref) => {
   return (
     <div className="font-lexend text-white mx-2 p-3 flex flex-row items-center justify-between">
       <div ref={ref.leftRef} className="flex items-center">
-      <Link to="/">
-        <MdOutlineSportsCricket size={40} />
+        {/* The Link component now serves as our hover group container */}
+        <Link to="/" className="flex items-center group">
+          {/* The icon wrapper uses group-hover to change color */}
+          <div className="transition-colors duration-200 text-white group-hover:text-blue-400">
+            <MdOutlineSportsCricket size={40} />
+          </div>
+          {/* The text now matches the icon's transition timing */}
+          <span className="mx-7 text-4xl transition-all duration-200 group-hover:text-blue-400 group-hover:scale-110 origin-left">
+            Cricketer App
+          </span>
         </Link>
-        <span className="mx-7 text-4xl">Cricketer App</span>
       </div>
       <div ref={ref.rightRef}>
         <SearchBar />
@@ -20,6 +27,5 @@ const Navbar = forwardRef((props, ref) => {
   );
 });
 
-Navbar.displayName = 'Navbar';
 
 export default Navbar;
