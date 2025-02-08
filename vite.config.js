cb1312b4236a0c8ec91f7c2637f5d7da7d7207ed
kanishkkant7 <kanishk.kant.7@gmail.com>
2025-02-08 21:42:36 +0530

    Reverted changes to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === 'UNRESOLVED_IMPORT') {
-          return
-        }
-        warn(warning)
-      }
-    }
-  }
 })
